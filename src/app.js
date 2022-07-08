f409f69483bef21f09e77f49fcf9c6fd46be600a
flaviapl <flavia_pala@hotmail.it>
2022-07-08 19:43:16 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io", "etc"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let m = 0; m < extension.length; m++) {
+          console.log(pronoun[i] + adj[x] + noun[y] + extension[m]);
+        }
+      }
+    }
+  }
 };
